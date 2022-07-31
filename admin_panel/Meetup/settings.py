0530051839ef88708a2403cfd9a96f902a431388
VneTraffiqua <safe72again@gmail.com>
2022-07-31 02:33:55 -0700

    Update settings.py

--- admin_panel/Meetup/settings.py
+++ admin_panel/Meetup/settings.py
@@ -31,8 +31,9 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['meetupwazzup.herokuapp.com']
+ALLOWED_HOSTS = ['meetupwazzup.herokuapp.com', 'localhost', '0.0.0.0']
 
+CSRF_TRUSTED_ORIGINS=['https://pythonmeetupbot.herokuapp.com']
 
 # Application definition
 
